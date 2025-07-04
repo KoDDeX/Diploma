bee1f4d31901f742f958ff4d7e4d414714e4ebcd
Ivan Bunko <kodexx@bk.ru>
2025-07-04 23:23:02 +0300

    Diploma: Добавлено представление главной страницы и шаблоны для автосервиса, обновлены настройки и маршруты

--- autoservice/settings.py
+++ autoservice/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'autoservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -120,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class LandingPageView(TemplateView):
+    """Представление для главной страницы сайта."""
+    template_name = 'core/landing.html'
+    extra_context = {
+        'title': '24автосервис',
+        'description': '24автосервис - решение для автомобилей и их водителей на все случаи жизни!',
+        # 'features': [
+        #     'Expert Mechanics',
+        #     'Affordable Prices',
+        #     'Quick Service',
+        #     'Customer Satisfaction Guaranteed'
+        # ]
+    }
